dfbefc0445b65704ddc1ddfc0f65a55eb810424e
Ryan Keener <ryan.keener@gmail.com>
2014-03-26 21:38:13 -0400

    added underscore

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,3 +8,6 @@ require('scripts/routes/*');
 require('scripts/views/*');
 require('scripts/router');
 
+console.log('Starting userCRUD!');
+if(typeof _ !== 'undefined') { console.log('underscore') } else { console.log('no underscore')}
+console.log('last: '+_.last([1,2,3,4]));
